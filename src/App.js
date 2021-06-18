02c13544b9537ca5690f5c68c74c3a994c228754
Chris Wright <chriswrightdev@gmail.com>
2021-06-18 10:20:57 +0100

    test fetch basic data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const festivalQueryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={festivalQueryClient}>
+      <TestFetch />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
+
+function TestFetch() {
+  //festivalData is a key to track the query
+  const { isLoading, error, data } = useQuery("festivalData", () =>
+    fetch(
+      "https://www.levenshulmecommunityfestival.co.uk/api_levfest_basic_data"
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  const festival = data[0];
+  console.log(data[0]);
+  return (
+    <div>
+      <h1>{festival.festival_title}</h1>
+      <p>{festival.festival_logo}</p>
+      <p>{festival.festival_intro}</p>
+    </div>
+  );
+}
